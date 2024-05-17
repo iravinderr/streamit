3c373c9df6c8b55f49ae92ec74ccc88a267fd224
iravinderr <imravinderr@gmail.com>
2024-05-17 14:27:56 +0530

    c

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/database.config.js";
+import app from "./app.js";
 
 
-connectDB();
+const PORT = process.env.PORT;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`SERVER IS LIVE AT PORT ${PORT}`);
+    })
+})
