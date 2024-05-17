3f13b250051f9904ed55d6def59089fc89462fff
iravinderr <imravinderr@gmail.com>
2024-05-17 20:47:27 +0530

    C

--- backend/src/utils/cloudinary.utils.js
+++ backend/src/utils/cloudinary.utils.js
@@ -5,11 +5,18 @@ import fs from "fs";
 const uploadToCloudinary = async (localFilePath) => {
     try {
 
-        if (!localFilePath) {
-            return null;
-        }
+        if (!localFilePath) return null;
         
+        const response = cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        return response;
     } catch (error) {
-        
+        fs.unlinkSync(localFilePath);
+        return null;
     }
 }
+
+
+export { uploadToCloudinary };
